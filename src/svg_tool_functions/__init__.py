5e8e5832ce15dc261665545f76688b523fc5be3b
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-05-25 19:24:57 -0300

    ordenando

--- src/svg_tool_functions/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import from_svgpath
-from . import from_text
-#from .core import *  # ou apenas funções específicas que você quer expor
-
--- src/svg_tool_functions/from_text/__init__.py
+++
@@ -1,2 +0,0 @@
-from .chars_to_svg import chars_to_svg
-
